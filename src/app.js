212b9c68618e4cdf82d6a6d61101b190815b3275
Junior Dugue <jrdugue@gmail.com>
2019-02-17 13:39:54 -0500

    ran babel and the presets for env and react to compile my jsx

--- src/app.js
+++ src/app.js
@@ -2,12 +2,7 @@ console.log("App is running");
 
 //JSX - JavaScript XML 
 
-// var template = <p>This is JSX from app.js!</p>
-
-var template = React.createElement("p", {
-  id: "app"
-}, "This is something NEW!");
-
+var template = <p>This is JSX from app.js!</p>
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
