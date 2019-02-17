b1008ad3907afa062edfae178b22804db33f5e58
Junior Dugue <jrdugue@gmail.com>
2019-02-17 12:17:04 -0500

    used babel to transpile the jsx code to understand react behind the scenes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,4 +2,12 @@ console.log("App is running");
 
 //JSX - JavaScript XML 
 
-var template = <p>This is JSX from app.js!</p>
+// var template = <p>This is JSX from app.js!</p>
+
+var template = React.createElement("p", {
+  id: "app"
+}, "This is something NEW!");
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
